4db6f6839c761599551cdfb000f1f2bd5ccd4b8f
mirko <mirko.bukilic@extraordinary.rs>
2020-08-17 22:29:36 +0200

    konekxcija

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,21 @@ Route::get('test', function (Request $request) {
     // $notification = array('message' => 'Test notification');
     // broadcast(new TestEvent($notification));
     // return response(['test notification']);
-    app('amqp')->publish('Message to direct exchange', 'routing-key', [
-        'exchange' => [
-            'type'    => 'direct',
-            'name'    => 'direct.exchange',
-        ],
+    app('amqp')->publish('hi to direct exchange', 'message', [
+        'connection' => 'konekcija', // should exists in amqp.php file
+        'channel_id' => 100, // int value or decided by the package
+        'exchange'   => [], // exchange configuration like amqp.php file
+        'message'    => [], // msg conf, will be passed along with msg
+    ]);
+});
+
+Route::get('konzum', function (Request $request) {
+    app('amqp')->consume(function ($message) {}, 'message', [
+        'connection' => 'konekcija', // should exists in amqp.php file
+        'channel_id' => 100, // int value or decided by the package
+        'exchange'   => [], // exchange configuration like amqp.php file
+        'queue'      => [], // queue configuration like amqp.php file,
+        'consumer'   => [], // consumer configuration like amqp.php file
+        'qos'        => [], // qos configuration like amqp.php file
     ]);
 });
